297758fdcd4fa9a1c7f40de4b1ee9720e41c8b0a
Miloš Stojanović <stojanovicmilos92@outlook.com>
2020-01-19 12:30:11 +0100

    Setup code linting and formatting.

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-import sirv from "sirv";
-import express from "express";
-import compression from "compression";
-import * as sapper from "@sapper/server";
+import sirv from 'sirv';
+import express from 'express';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === "development";
+const dev = NODE_ENV === 'development';
 
 const app = express();
 app
   .use(
     compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleware()
+    sirv('static', { dev }),
+    sapper.middleware(),
   )
   .listen(PORT, err => {
-    if (err) console.log("error", err);
+    // eslint-disable-next-line no-console
+    if (err) console.log('error', err);
   });
